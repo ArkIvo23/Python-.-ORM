f1f820186cbde0ea4d4da9d2c3a84fc71a56e1d8
Arkadii Ivonin <ark811g@gmail.com>
2024-02-14 21:44:56 +0300

    Recording file - Task1andTask2.py

--- main.Task1andTask2.py
+++ main.Task1andTask2.py
@@ -1,7 +1,7 @@
-import sqlalchemy
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Relationship
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import or_
 
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/ORMDB')
 Base = declarative_base()
@@ -12,8 +12,14 @@ class Book(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String)
     publisher_id = Column(Integer, ForeignKey('publishers.id'))
-    publisher = Relationship('Publisher', backref='books')
+    publisher = relationship('Publisher', backref='books')
 
+
+class BookStore(Base):
+    __tablename__ = 'bookstores'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    location = Column(String)
 
 class Publisher(Base):
     __tablename__ = 'publishers'
@@ -31,22 +37,32 @@ class Sale(Base):
     __tablename__ = 'sales'
     id = Column(Integer, primary_key=True)
     book_id = Column(Integer, ForeignKey('books.id'))
-    book = Relationship('Book', backref='sales')
+    book = relationship('Book', backref='sales')
     shop_id = Column(Integer, ForeignKey('shops.id'))
-    shop = Relationship('Shop', backref='sales')
+    shop = relationship('Shop', backref='sales')
     date = Column(DateTime)
     price = Column(Integer)
+
+class PublisherShop(Base):
+    __tablename__ = 'publisher_shops'
+    id = Column(Integer, primary_key=True)
+    publisher_id = Column(Integer, ForeignKey('publishers.id'))
+    shop_id = Column(Integer, ForeignKey('shops.id'))
 
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
-def get_shops(publisher_name):
-    publisher = session.query(Publisher).filter(Publisher.name == publisher_name).first()
+def get_shops(publisher_input):
+    publisher = session.query(Publisher).filter(or_(Publisher.name == publisher_input, Publisher.id == publisher_input)).first()
     if publisher is None:
         return None
-    return publisher.shops
+    publisher_shops = session.query(PublisherShop).filter(PublisherShop.publisher_id == publisher.id).all()
+    shops = [session.query(Shop).filter(Shop.id == ps.shop_id).first() for ps in publisher_shops]
+    return shops
+
+
 
 
 def print_sales(shops):
@@ -55,7 +71,7 @@ def print_sales(shops):
         for sale in sales:
             print(f"{sale.book.title} | {shop.name} | {sale.price} | {sale.date}")
 
-
+Base.metadata.create_all(engine)
 if __name__ == '__main__':
     publisher_name = input("Enter publisher name: ")
     shops = get_shops(publisher_name)

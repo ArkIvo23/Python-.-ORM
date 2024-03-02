385690a69a6d02db7c68af626dc502e132b260b6
Arkadii Ivonin <ark811g@gmail.com>
2024-03-02 16:04:55 +0300

    Recording020324

--- Recording File Task 1 and Task 2.py
+++ Recording File Task 1 and Task 2.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, Date
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
 
@@ -10,13 +9,6 @@ class Publisher(Base):
     id_publ = Column('id_publ', Integer, primary_key=True)
     name = Column('name', String(length=40))
 
-    def __init__(self, id_publ, name):
-        self.id_publ = id_publ
-        self.name = name
-
-    def __repr__(self):
-        return f'({self.id_publ}) {self.name}'
-
 
 class Book(Base):
     __tablename__ = 'book'
@@ -24,26 +16,11 @@ class Book(Base):
     title = Column('title', String(length=40))
     id_publ = Column('id_publ', Integer, ForeignKey('publisher.id_publ'))
 
-    def __repr__(self):
-        return f'{self.title}'
-
-    def __init__(self, id_book, title, id_publ):
-        self.id_book = id_book
-        self.title = title
-        self.id_publ = id_publ
-
 
 class Shop(Base):
     __tablename__ = 'shop'
     id_shop = Column('id_shop', Integer, primary_key=True)
     name = Column('name', String(length=40))
-
-    def __init__(self, id_shop, name):
-        self.id_shop = id_shop
-        self.name = name
-
-    def __repr__(self):
-        return f'{self.name}'
 
 
 class Stock(Base):
@@ -52,12 +29,6 @@ class Stock(Base):
     id_book = Column('id_book', Integer, ForeignKey('book.id_book'))
     id_shop = Column('id_shop', Integer, ForeignKey('shop.id_shop'))
     count = Column('count', Integer)
-
-    def __init__(self, id_stok, id_book, id_shop, count):
-        self.id_stock = id_stok
-        self.id_book = id_book
-        self.id_shop = id_shop
-        self.count = count
 
 
 class Sale(Base):
@@ -68,29 +39,29 @@ class Sale(Base):
     id_stock = Column('id_stock', Integer, ForeignKey('stock.id_stock'))
     count = Column('count', Integer)
 
-    def __init__(self, id_price, price, date_sale, id_stock, count):
-        self.id_price = id_price
-        self.price = price
-        self.date_sale = date_sale
-        self.id_stock = id_stock
-        self.count = count
-
-    def __repr__(self):
-        return f'{self.price} | {self.date_sale}'
-
 
 engine = create_engine('postgresql://postgres:1109@localhost:5432/dbalchemy')
-# Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-request = input()
-result = session.query(Book, Shop, Sale).filter(Publisher.name == request).filter(
-    Publisher.id_publ == Book.id_publ).filter(Book.id_publ == Stock.id_book).filter(
-    Stock.id_shop == Shop.id_shop).filter(Stock.id_stock == Sale.id_stock).all()
-for r in result:
-    print(f'{r[0]} | {r[1]} | {r[2]}')
+
+def get_shops(input_data):
+    query = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).select_from(Shop). \
+        join(Stock).join(Book).join(Publisher).join(Sale)
+
+    if input_data.isdigit():
+        result = query.filter(Publisher.id_publ == input_data).all()
+    else:
+        result = query.filter(Publisher.name == input_data).all()
+
+    for title, shop_name, price, date_sale in result:
+        print(f"{title: <40} | {shop_name: <10} | {price: <8} | {date_sale.strftime('%d-%m-%Y')}")
+
+
+if __name__ == '__main__':
+    user_input = input("Enter publisher name or ID: ")
+    get_shops(user_input)
 
 session.close()
